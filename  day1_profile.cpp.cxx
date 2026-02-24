69d286336fb03f4df47d5b4276ca1f7514e7d853
Suhani Singh <suhanisingh7009@gmail.com>
2026-02-24 20:35:31 +0530

    "Project: Basic Profile Generator in C++"
    "Today is Day 1 of my C++ journey. I created a basic profile generator that takes user input like Name, Age, and 12th Percentage. Learned about variables, data types, and cin/cout."

---
+++  day1_profile.cpp.cxx
@@ -0,0 +1,35 @@
+
+
+#include  <iostream>
+using namespace std;
+
+int main( )  {
+	// Variables define krna
+	string name;
+	int age;
+	float percentage;
+	
+    cout << "--- Future AI-ML Engineer Profile ----\n";
+         
+          	         	  
+       // User se input lena
+       cout << "Enter your Name:  " ;
+       cin >> name ;
+       
+       cout << "Enter your Age: " ;
+       cin >> age ;
+       
+       cout <<"Enter your 12th percentage: ";       cin >> percentage ;
+       
+       // Output dikhana
+       cout  << "\n--- Profile Summary ---\n" ;
+       cout  << "Name: " << name << endl ;
+       cout  << "Age: " << age << " years " <<            endl ;
+       cout <<"Target Score:  " << percentage << "%" << endl ;
+       cout << "Status :  Counting Journey Started! " << endl;
+       
+       return 0;
+       
+}
+       
+	         	
